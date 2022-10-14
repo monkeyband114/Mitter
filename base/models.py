e4a4d05a2ef9c0e06ad0b61f83dec72e631380ef
Michael Voke <gadocer54@gmail.com>
2022-10-14 14:35:09 +0100

    modified:   base/models.py

--- base/models.py
+++ base/models.py
@@ -10,9 +10,7 @@ class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
-    
     avatar = models.ImageField(null=True, default="avatar.svg")
-    
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
